d79f562cd362fdf6a795aae83283f3d36e2999b3
minachanchan <ayaka.minagawa@di-v.co.jp>
2024-07-04 23:50:30 +0900

    turbo false

--- app/javascript/channels/comment_channel.js
+++ app/javascript/channels/comment_channel.js
@@ -1,31 +1,34 @@
 import consumer from "channels/consumer"
 
-if(location.pathname.match(/\/items\/\d/)){
-  console.log("読み込み完了")
+// Turboのライフサイクルイベントを使用して、DOMContentLoadedの代わりに処理
+document.addEventListener("turbo:load", function() {
+  if(location.pathname.match(/\/items\/\d/)){
+    console.log("読み込み完了")
 
-  consumer.subscriptions.create({
-    channel: "CommentChannel",
-    item_id: location.pathname.match(/\d+/)[0]
-  }, {
-    connected() {
-      // Called when the subscription is ready for use on the server
-    },
+    consumer.subscriptions.create({
+      channel: "CommentChannel",
+      item_id: location.pathname.match(/\d+/)[0]
+    }, {
+      connected() {
+        // Called when the subscription is ready for use on the server
+      },
 
-    disconnected() {
-      // Called when the subscription has been terminated by the server
-    },
+      disconnected() {
+        // Called when the subscription has been terminated by the server
+      },
 
-    received(data) {
-      console.log(data) //追加
-      const html = `
-      <div class="comment">
-        <p class="user-info">${data.user.nickname}： </p>
-        <p>${data.comment.text}</p>
-      </div>`
-      const comments = document.getElementById("comments")
-      comments.insertAdjacentHTML('beforeend', html)
-      const commentForm = document.getElementById("comment-form")
-      commentForm.reset();
-    }
-  })
-}
+      received(data) {
+        console.log(data) //追加
+        const html = `
+        <div class="comment">
+          <p class="user-info">${data.user.nickname}： </p>
+          <p>${data.comment.text}</p>
+        </div>`
+        const comments = document.getElementById("comments")
+        comments.insertAdjacentHTML('beforeend', html)
+        const commentForm = document.getElementById("comment-form")
+        commentForm.reset();
+      }
+    })
+  }
+})
